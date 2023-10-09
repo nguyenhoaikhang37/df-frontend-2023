bd6f2a8fc5bdeb88d4df09ff1ed1fff08b8af32a
nguyenhoaikhang37 <khangnguyennhkdev@gmail.com>
2023-10-09 15:24:43 +0700

    bugfix: fix deploy

--- assignment-5/src/app/not-found.tsx
+++ assignment-5/src/app/not-found.tsx
@@ -3,7 +3,7 @@ import { Button } from '../components/common'
 
 export default function NotFound() {
   return (
-    <div className={`flex h-screen flex-col items-center justify-center`}>
+    <div className="flex h-screen flex-col items-center justify-center">
       <div className="text-center">
         <h1 className="text-8xl font-bold">404</h1>
         <p className="mt-2">Page not found</p>
