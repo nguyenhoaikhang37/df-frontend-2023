d37fa1a7221b855d5ec25684c7e2533a3cf8df56
nguyenhoaikhang37 <khangnguyennhkdev@gmail.com>
2023-10-10 17:23:02 +0700

    bugfix: fix deploy

--- assignment-6/src/utils/functions/auth.ts
+++ assignment-6/src/utils/functions/auth.ts
@@ -2,8 +2,12 @@ import { UserProfile } from '../../types'
 import { StorageKeys } from '../constants'
 
 export function getAccessToken(): UserProfile | null {
+  if (typeof window === 'undefined') {
+    return null
+  }
+
   try {
-    return JSON.parse(localStorage.getItem(StorageKeys.JWT) || '')
+    return JSON.parse(window.localStorage.getItem(StorageKeys.JWT) || '')
   } catch (error) {
     return null
   }
